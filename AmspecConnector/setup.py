4183a231c82d2f85bf953f2f0b28b0df27bd97a0
Ujjawalb1 <ujjawalb@engineosol.com>
2025-07-03 15:03:18 +0530

    version change

--- AmspecConnector/setup.py
+++ AmspecConnector/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name="amspec_einvoice_package",
-    version="0.1.0",
+    version="0.1.2",
     author="Moksh Baweja",
     author_email="mokshb@engineosol.com",
     description = "package for einvoices",
